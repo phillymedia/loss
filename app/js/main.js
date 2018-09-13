3427de8c797f6c1eee8302a88a88464f33875c44
Garland <garlandpotts1@gmail.com>
2018-09-13 15:57:17 -0400

    Adding galleries and vignette styles

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,30 @@
-$(document).ready(function(){
+$(document).ready(function() {
+    $(".left-pullquote").append('<svg alignment="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><path d="M31.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L127.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L201.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34z"></path></svg>')
+
+    require("./gallery")
+
+    if ($( window ).width() <= 700) {
+
+
+        $(".vignette").each(function() {
+            var $this = $(this);
+            var cutoffTop  = $this.find("p").eq(2).offset().top;
+            var vignetteTop = $this.offset().top;
+
+            $this.find(".container").css("max-height",cutoffTop - vignetteTop + 50 + "px")
+
+            $this.find('.expandButton').click(function(){
+                $this.find(".container").css("max-height","unset")
+                $this.removeClass("afterShaddow");
+                $this.find('.expandButton').css("display","none")
+
+            })
+
+        })
+    } else {
+        $(".expandButton").hide();
+        $(".afterShaddow").removeClass("afterShaddow");
+    }
+
+
 });
--- gulp-tasks/nunjucks-build.js
+++ gulp-tasks/nunjucks-build.js
@@ -1,8 +1,17 @@
 var nunjucksRender = require('gulp-nunjucks-render');
+var data = require("gulp-data");
+
 
 module.exports = function (gulp, plugins) {
     return function () {
        gulp.src('app/**/*.+(html|nunjucks)')
+       .pipe(data({
+           gal1: require("../app/gal1.json"),
+           gal2: require("../app/gal2.json"),
+           gal3: require("../app/gal3.json"),
+           gal4: require("../app/gal4.json"),
+           gal5: require("../app/gal5.json")
+       }))
       .pipe(nunjucksRender({
            path: ['app/templates']
         }))
